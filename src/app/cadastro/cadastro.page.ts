a22a08d1e76f5eb2c96327f7763ecaaf95fb1f59
FalapaxD <felipe.fllor@gmail.com>
2023-08-30 21:47:57 -0300

    local storage

---
+++ src/app/cadastro/cadastro.page.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { LocalStorageService } from '../local-storage.service';
+
+@Component({
+  selector: 'app-cadastro',
+  templateUrl: './cadastro.page.html',
+  styleUrls: ['./cadastro.page.scss'],
+})
+export class CadastroPage implements OnInit {
+  public materia: string = '';
+  public descricao: string = '';
+  public datahora: string = '';
+
+  constructor(public local_storage: LocalStorageService) {}
+
+  ngOnInit() {}
+
+  salvar() {
+    let compromisso = {
+      materia:this.materia,
+      descricao:this.descricao,
+      datahora:this.datahora
+    };
+    
+    this.local_storage.salvar('compromisso',compromisso);
+    alert('Salvo com sucesso');
+  }
+}
